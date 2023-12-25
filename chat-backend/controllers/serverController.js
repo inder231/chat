00372f5a1d468cd413f49b0dce374147ec3f8549
inder231 <inder39811@gmail.com>
2023-12-25 22:08:53 +0530

    ref: sharing express session with socket io

---
+++ chat-backend/controllers/serverController.js
@@ -0,0 +1,30 @@
+const redisClient = require("../config/redis");
+const RedisStore = require("connect-redis").default; // pass session here..
+const session = require("express-session");
+require("dotenv").config();
+
+const sessionMiddleware = session({
+  secret: process.env.COOKIE_SECRET,
+  credentials: true,
+  name: "sid",
+  store: new RedisStore({ client: redisClient }),
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.ENV === "production",
+    httpOnly: true,
+    // it allow cookies to be set when we both are no same domain, but in production we need to be none and in development to be lax
+    sameSite: process.env.ENV === "production" ? "none" : "lax",
+    maxAge: 1000 * 60 * 60 * 1, // cookie expired after 1hour
+  },
+});
+
+const wrap = (expressMiddleware) => (socket, next) =>
+  expressMiddleware(socket.request, {}, next);
+
+const corsConfig = {
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST"],
+  }
+module.exports = { sessionMiddleware,wrap,corsConfig };
--- chat-backend/index.js
+++ chat-backend/index.js
@@ -4,55 +4,35 @@ const helmet = require("helmet");
 const cors = require("cors");
 const createError = require("http-errors");
 const morgan = require("morgan");
-const session = require("express-session");
-require("dotenv").config();
 const authRouter = require("./routes/auth.routes");
 const { connection } = require("./config/db");
-const redisClient = require("./config/redis");
-const RedisStore = require("connect-redis").default; // pass session here..
+const {
+  sessionMiddleware,
+  wrap,
+  corsConfig,
+} = require("./controllers/serverController");
 
 const app = express();
 
 const server = require("http").createServer(app);
 
 const io = new Server(server, {
-  cors: {
-    origin: "http://localhost:5173",
-    credentials: true,
-  },
+  cors: corsConfig,
 });
 
 app.use(morgan("dev"));
 app.use(helmet());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST"],
-  })
-);
+app.use(cors(corsConfig));
 app.use(express.json());
-app.use(
-  session({
-    secret: process.env.COOKIE_SECRET,
-    credentials: true,
-    name: "sid",
-    store: new RedisStore({ client: redisClient }),
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.ENV === "production",
-      httpOnly: true,
-      // it allow cookies to be set when we both are no same domain, but in production we need to be none and in development to be lax
-      sameSite: process.env.ENV === "production" ? "none" : "lax",
-      maxAge: 1000 * 60 * 5, // cookie expired after 5min
-    },
-  })
-);
+app.use(sessionMiddleware);
 
 app.use("/auth", authRouter);
 
-io.on("connect", (socket) => {});
+// <------  ----- io -----  ------>
+io.use(wrap(sessionMiddleware));
+io.on("connect", (socket) => {
+  console.log(socket.request.session);
+});
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
